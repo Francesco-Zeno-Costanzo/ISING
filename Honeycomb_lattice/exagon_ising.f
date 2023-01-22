be349ac73acc97141827ab91e0177e9dde4aeb0e
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2023-01-22 15:06:58 +0100

    secondi e terzi vicini

--- Honeycomb_lattice/exagon_ising.f
+++ Honeycomb_lattice/exagon_ising.f
@@ -1,5 +1,30 @@
       program honeycomb_ising
-	  
+      
+C===============================================================
+C Il codice simula un modello di ising su reticolo esagone con
+C accoppiamenti fino a terzi vicini; l'hamiltoniana è:
+C
+C H = - J_0*sum(s_i*s_j) + J_1*sum(s_i*s_j)  - J_2*sum(s_i*s_j)
+C           primi vicini       secondi vicini      terzi vicini
+C
+C Le costanti di accoppiamento sia assumono essere positive.
+C Facciamo un esempio per chiarire, supponendo L=5 e scegliando
+C un sito a caso del reticolo (e.g. 17) si avrà la struttura:
+C
+C                25          27
+C          24          26          28
+C
+C          15          17          19
+C                16          18
+C          
+C                7           9
+C                      8
+C
+C I primi   vicini di 17 sono: 16, 18 e 26
+C I secondi vicini di 17 sono: 7, 9, 25, 27, 15 e 19
+C I terzi   vicini di 17 sono: 8, 24 e 28
+C===============================================================	
+  
       include "parameter.f"
 C===============================================================
 C file che permette di cambiare più facilmente i parametri della
@@ -14,7 +39,7 @@ C===============================================================
       call ranstart
 
       open(1, file='init.txt',status='old')		      !file coi parametri
-      open(2, file='dati/dati45.dat',status='unknown')	!file coi risultati
+      open(2, file='anti_dati/dati10.dat',status='unknown')	!file coi risultati
 	
       read(1,*) misure           !numero di misure
       read(1,*) i_dec            !updating fra una misura e l'altra
@@ -22,8 +47,10 @@ C===============================================================
       read(1,*) bmin 		   !temperatura inversa minima
       read(1,*) bmax		   !temperatura inversa missima
       read(1,*) npassi	         !numero di temperature
-      read(1,*) J_acc            !costante di accoppiamento
-       
+      read(1,*) J_0              !costante di accoppiamento primi   vicini
+      read(1,*) J_1              !costante di accoppiamento secondi vicini
+      read(1,*) J_2              !costante di accoppiamento terzi   vicini
+
       write(2, *) misure	   !scrivo quantità che seriviranno
       write(2, *) nvol	         !nell'analisi
       write(2, *) npassi
@@ -35,7 +62,7 @@ C===============================================================
       !ogni blocco corrisponde ad una temperatura diversa
       !ci sono npassi blocchi, ciascuno lungo misure
       
-      write(*,*) 'Taglia del reticolo: ',nlatt 
+      write(*,*) 'Taglia del reticolo: ',nlatt
 	
       do k = 1, npassi		 !ciclo sulle temperature
 	
@@ -51,7 +78,7 @@ C===============================================================
               call magnetizzazione(mag)	   !misurazione delle osservabili
               call energia(ene)
 
-              write(2,*) abs(mag), ene	   !salvataggio risultati
+              write(2,*) abs(mag), ene	         !salvataggio risultati
 
           enddo
 
@@ -85,26 +112,101 @@ C============================================================================
       subroutine bordi() 		
 	   
       include "parameter.f"
+      
+      !primi vicini
+      if (J_0 /= 0) then
       	
-      do i = 1, nvol
+          do i = 1, nvol
       
-          n1(i) = i + 1		                    !codizioni per ogni sito
-          n2(i) = i - 1
+              nn1(i) = i + 1		                  !codizioni per ogni sito
+              nn2(i) = i - 1
       	  
-          if (modulo(i, 2) == 0) then             !Condizioni che regolano
-      	  n3(i) = i - 2*nlatt + 1             !l'esagonalità del reticolo.
-          else                                    !Il reticolo è un array ma le
-      	  n3(i) = i + 2*nlatt - 1             !interazioni con questi vicini
-          endif                                   !lo mappano in un reticolo esagonale
+              if (modulo(i, 2) == 0) then             !Condizioni che regolano
+      	      nn3(i) = i - 2*nlatt + 1            !l'esagonalità del reticolo.
+              else                                    !Il reticolo è un array ma le
+      	      nn3(i) = i + 2*nlatt - 1            !interazioni con questi vicini
+              endif                                   !lo mappano in un reticolo esagonale
       	  
-          if ((n3(i) < 0).or.(n3(i)>nvol)) then   !condizioni, periodico-elicoidali
-              n3(i) = modulo(n3(i), nvol)	        !sopra e sotto identificati, mentre
-          endif                                   !la riga i-esima si attacca alla i+1 -esima
+              if ((nn3(i) < 0).or.(nn3(i)>nvol)) then !condizioni, periodico-elicoidali
+                  nn3(i) = modulo(nn3(i), nvol)	      !sopra e sotto identificati, mentre
+              endif                                   !la riga i-esima si attacca alla i+1 -esima
       	  
-      enddo
+          enddo
       
-      n1(nvol) = 1			              !problemi agli estremi
-      n2(1) = nvol
+          nn1(nvol) = 1			              !problemi agli estremi
+          nn2(1) = nvol
+      endif
+      
+      !secondi vicini
+      if (J_1 /= 0) then
+      	
+          do i = 1, nvol
+          
+              nnn1(i) = i - 2*nlatt
+              nnn2(i) = i - 2*nlatt + 2
+              nnn3(i) = i + 2*nlatt - 2
+              nnn4(i) = i + 2*nlatt
+              nnn5(i) = i + 2
+              nnn6(i) = i - 2
+              
+              if ((nnn1(i) < 0).or.(nnn1(i)>nvol)) then   
+                  nnn1(i) = modulo(nnn1(i), nvol)
+              endif
+              
+              if ((nnn2(i) < 0).or.(nnn2(i)>nvol)) then   
+                  nnn2(i) = modulo(nnn2(i), nvol)
+              endif
+              
+              if ((nnn3(i) < 0).or.(nnn3(i)>nvol)) then   
+                  nnn3(i) = modulo(nnn3(i), nvol)
+              endif
+              
+              if ((nnn4(i) < 0).or.(nnn4(i)>nvol)) then   
+                  nnn4(i) = modulo(nnn4(i), nvol)
+              endif
+              
+              if ((nnn5(i) < 0).or.(nnn5(i)>nvol)) then   
+                  nnn5(i) = modulo(nnn5(i), nvol)
+              endif
+              
+              if ((nnn6(i) < 0).or.(nnn6(i)>nvol)) then   
+                  nnn6(i) = modulo(nnn6(i), nvol)
+              endif
+              
+          enddo
+
+      endif
+      
+      !terzi vicini
+      if (J_2 /= 0) then
+      	
+          do i = 1, nvol
+          
+              if (modulo(i, 2) == 0) then
+      	      nnnn1(i) = i + 2*nlatt - 1
+      	      nnnn2(i) = i - 2*nlatt - 1
+      	      nnnn3(i) = i - 2*nlatt + 3
+              else
+      	      nnnn1(i) = i - 2*nlatt + 1
+      	      nnnn2(i) = i + 2*nlatt + 1
+      	      nnnn3(i) = i + 2*nlatt - 3
+              endif
+              
+              if ((nnnn1(i) < 0).or.(nnnn1(i)>nvol)) then  
+                  nnnn1(i) = modulo(nnnn1(i), nvol)
+              endif
+              
+              if ((nnnn2(i) < 0).or.(nnnn2(i)>nvol)) then  
+                  nnnn2(i) = modulo(nnnn2(i), nvol)
+              endif
+              
+              if ((nnnn3(i) < 0).or.(nnnn3(i)>nvol)) then  
+                  nnnn3(i) = modulo(nnnn3(i), nvol)
+              endif
+              
+          enddo
+
+      endif
       	
       return
       end
@@ -141,25 +243,53 @@ C============================================================================
       include "parameter.f"
       
       do j = 1, nvol				      !ciclo su tutti i siti
-		
+	    
+	    F4 = 0
           i = int(nvol*ran2()+1)	            !scelta random di un sito
-		
-          i1 = n1(i)			            !calcolo dei primi vicini
-          i2 = n2(i)
-          i3 = n3(i)
-
-          F = campo(i1) + campo(i2) + campo(i3) !sommo i primi vicini
-          F = beta*(F + bext)		            !aggiungo eventuale campo esterno
+	    
+	    if (J_0 /= 0) then
+              
+              i1 = nn1(i)			      !calcolo dei primi vicini
+              i2 = nn2(i)
+              i3 = nn3(i)
+              F1 = campo(i1) + campo(i2) + campo(i3)
+              F4 = F4 -J_0*F1
+              
+          endif
+          if (J_1 /= 0) then
+          
+              l1 = nnn1(i)			      !calcolo dei secondi vicini
+              l2 = nnn2(i)
+              l3 = nnn3(i)
+              l4 = nnn4(i)
+              l5 = nnn5(i)
+              l6 = nnn6(i)
+              F2 = campo(l1) + campo(l2) + campo(l3) + campo(l4) +
+     &             campo(l5) + campo(l6)
+              F4 = F4 + J_1*F2
+     
+          endif
+          if (J_2 /= 0) then
+          
+              k1 = nnnn1(i)			      !calcolo dei terzi vicini
+              k2 = nnnn2(i)
+              k3 = nnnn3(i)
+              F3 = campo(k1) + campo(k2) + campo(k3)
+              F4 = F4 - J_2*F3
+              
+          endif
+          
+          F = beta*(F4 + bext)		            !aggiungo eventuale campo esterno
      		
           ispin = campo(i)
      		
-          p =  exp(-2.0*J_acc*ispin*F)		!probabilità di accettare la mossa
+          p =  exp(-2.0*ispin*F)		!probabilità di accettare la mossa
      		
-          x = ran2()			            !numero casuale per il test
+          x = ran2()			      !numero casuale per il test
      		
-          if(x < p) then			      !test di accettanza
-              campo(i) = -ispin	            !se F è negativo il test è
-     	    endif				            !passato di default
+          if(x < p) then			!test di accettanza
+              campo(i) = -ispin	      !se F è negativo il test è
+     	    endif				      !passato di default
 	
       enddo
       
@@ -178,11 +308,19 @@ C============================================================================
       
       mag = 0			  !inizializzo la variabile
       
-      do i = 1, nvol		  !ciclo su tutto il reticolo
+      if (J_1 <= 0) then 
+          do i = 1, nvol		  !ciclo su tutto il reticolo
       
-          mag = mag + campo(i)  !e sommo ogni sito
+              mag = mag + campo(i)  !e sommo ogni sito
 
-      enddo
+          enddo
+      else 
+          do i = 1, nvol		  !ciclo su tutto il reticolo
+      
+              mag = mag + ((-1)**i)*campo(i)  !e sommo ogni sito
+
+          enddo
+      endif
       
       return
       end
@@ -201,14 +339,42 @@ C============================================================================
       
       do i = 1, nvol				      !ciclo su tutti i siti
 
-          i1 = n1(i)			            !calcolo dei primi vicini
-          i2 = n2(i)
-          i3 = n3(i)
-		
-          F = campo(i1) + campo(i2) + campo(i3) !sommo i primi vicini
+          F4 = 0
+	    
+	    if (J_0 /= 0) then
+              
+              i1 = nn1(i)			      !calcolo dei primi vicini
+              i2 = nn2(i)
+              i3 = nn3(i)
+              F1 = campo(i1) + campo(i2) + campo(i3)
+              F4 = F4 - J_0*F1
+              
+          endif
+          if (J_1 /= 0) then
+          
+              l1 = nnn1(i)			      !calcolo dei secondi vicini
+              l2 = nnn2(i)
+              l3 = nnn3(i)
+              l4 = nnn4(i)
+              l5 = nnn5(i)
+              l6 = nnn6(i)
+              F2 = campo(l1) + campo(l2) + campo(l3) + campo(l4) +
+     &             campo(l5) + campo(l6)
+              F4 = F4 + J_1*F2
+     
+          endif
+          if (J_2 /= 0) then
+          
+              k1 = nnnn1(i)			      !calcolo dei terzi vicini
+              k2 = nnnn2(i)
+              k3 = nnnn3(i)
+              F3 = campo(k1) + campo(k2) + campo(k3)
+              F4 = F4 - J_2*F3
+              
+          endif
  
-          ene = ene - 0.5*J_acc*F*campo(i)	!0.5 per non sovracontare
-          ene = ene - bext*campo(i)      	      !eventuale campo esterno
+          ene = ene - 0.5*F4*campo(i)	!0.5 per non sovracontare
+          ene = ene - bext*campo(i)      	!eventuale campo esterno
 
       enddo
 
